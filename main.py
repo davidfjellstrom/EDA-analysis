4e946edbcc260827b52d272bf421fe8dd15b276a
davidfjellstrom <56550285+davidfjellstrom@users.noreply.github.com>
2026-04-13 18:39:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from rich.table import Table
+from rich.console import Console
+
+df = pd.read_csv('/Users/davidfjellstrom/Downloads/messy_IMDB_dataset.csv', sep=';')
+
+console = Console()
+table = Table(show_header=True, header_style="bold magenta")
+
+for col in df.columns:
+    table.add_column(col)
+
+for _, row in df.iterrows():
+    table.add_row(*[str(v) for v in row])
+
+console.print(table)
